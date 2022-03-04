6a2bb174e1d37a24c080bf91f2f57174dee3245b
Carlos Ibarra <carlos.ibarra@teksystemsgs.com>
2022-03-04 11:09:03 -0800

    Updated license file and license header in the classes

--- src/main/java/com/softwareonpurpose/indentmanager/IndentManager.java
+++ src/main/java/com/softwareonpurpose/indentmanager/IndentManager.java
@@ -1,6 +1,6 @@
 
 /*
-  Copyright 2021 Craig A. Stockton
+  Copyright 2021 - 2022 Craig A. Stockton
   <p>
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
